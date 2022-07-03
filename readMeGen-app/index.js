de17801b8d94a8d8bbcb8838829043d3aed5b9e9
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-07-02 22:26:00 -0400

    working pairs

--- readMeGen-app/index.js
+++ readMeGen-app/index.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 inquirer.prompt([
     {
         type: 'input',
-        message: 'Project Title?',
+        message: 'Project Name?',
         name: 'README1',
     },
     {
@@ -21,52 +21,61 @@ inquirer.prompt([
     },
     {
         type: 'input',
-        message: 'How does it work?',
+        message: 'How does it work??',
         name: 'README4',
+    },
+    {
+        type: 'input',
+        message: 'License',
+        name: 'README5',
     } // test
 ])
 .then((answers) => {
-    let question1 = `## Project Title?\n\n`
+    
+    let question1 = `## Project Name?\n\n`
     let question2 = `## Description?\n\n`
     let question3 = `## What Problem does it Solve?\n\n`
     let question4 = `## How does it work?\n\n`
-    const questionsAll = [question1, question2, question3, question4];
+    let question5 = `## License\n\n`
+    //const questionsAll = [question1, question2, question3, question4];
 
     let answer1 = answers.README1
     let answer2 = answers.README2
     let answer3 = answers.README3 
     let answer4 = answers.README4 
-    const answersAll = [answer1, answer2, answer3, answer4];
-    //let pairs = `${question2}${answer2}`
+    let answer5 = answers.README5 
+    //const answersAll = [answer1, answer2, answer3, answer4];
+    let pairs1 = `${question1}${answer1}\n`
+    let pairs2 = `${question2}${answer2}\n`
+    let pairs3 = `${question3}${answer3}\n`
+    let pairs4 = `${question4}${answer4}\n`
+    let pairs5 = `${question5}${answer5}\n`
+    let pairs = [pairs1, pairs2, pairs3, pairs4, pairs5]
     let welcomeNote = `# Welcome to ${answer1} App!\n\n`
 
     fs.writeFile('README.md', welcomeNote, (err) => {
         err ? console.error(err) : console.log('Success!')
     });
-    
-    for (const question of questionsAll){
+
+    for (const pair of pairs){
         //createFormat(question, answersAll) 
-        fs.appendFile('README.md', question, (err) => {
+        fs.appendFile('README.md', pair, (err) => {
             err ? console.error(err) : console.log('Success!')
         });
-
-        // answersAll.forEach(answer => {
-        //     let answersStr = answer
-        //     fs.appendFile('README.md', ('\n', answersStr, '\n'), (err) => {
-        //         err ? console.error(err) : console.log('Answer!')
-        //     });
-        // })
-        // for (const answer of answersAll){
-        //     fs.appendFile('README.md', ('\n',answer, '\n'), (err) => {
-        //         err ? console.error(err) : console.log('Answer!')
-        //     });
-        // }
     }
     
 })
 .catch((err) => {
     console.log(err)
 })
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+
 
 // TODO: Create an array of questions for user input
 // let question1 = `##Project Title?\n`
@@ -120,8 +129,6 @@ inquirer.prompt([
 // fs.appendFile('README.md', JSON.stringify(data, null, '\t'), (err) => {
 //     err ? console.error(err) : console.log(data)
 // });
-
-
 
 // // TODO: Create a function to initialize app
 // function init() {}
