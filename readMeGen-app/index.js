fe3b08088de65e44ec119b5903fb996dc969ca37
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-07-04 01:48:45 -0400

    add link http/img -- readmegen

--- readMeGen-app/index.js
+++ readMeGen-app/index.js
@@ -1,9 +1,11 @@
 console.log(`Program Started`)
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
-const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
+//const fs = require('fs')
 
-inquirer.prompt([ // ADD LINKS readme Markdown [title](https://www.example.com) ![alt text](image.jpg)
+// const readMEGen = inquirer.prompt([])
+inquirer.prompt([ 
     {
         type: 'input',
         message: 'Project Name?',
@@ -26,43 +28,22 @@ inquirer.prompt([ // ADD LINKS readme Markdown [title](https://www.example.com) 
     },
     {
         type: 'input',
-        message: 'License',
+        message: 'Deployed Link: [title](https://www.example.com)',
         name: 'README5',
-    } // test
+    },
+    {
+        type: 'input',
+        message: 'Image Link: ![alt text](image.jpg)',
+        name: 'README6',
+    },
+    {
+        type: 'input',
+        message: 'License',
+        name: 'README7',
+    },  // test
 ])
 .then((answers) => {
-    
-    let question1 = `## Project Name?\n\n`
-    let question2 = `## Description?\n\n`
-    let question3 = `## What Problem does it Solve?\n\n`
-    let question4 = `## How does it work?\n\n`
-    let question5 = `## License\n\n`
-    //const questionsAll = [question1, question2, question3, question4];
-
-    let answer1 = answers.README1
-    let answer2 = answers.README2
-    let answer3 = answers.README3 
-    let answer4 = answers.README4 
-    let answer5 = answers.README5 
-    //const answersAll = [answer1, answer2, answer3, answer4];
-    let pairs1 = `${question1}${answer1}\n\n`
-    let pairs2 = `${question2}${answer2}\n\n`
-    let pairs3 = `${question3}${answer3}\n\n`
-    let pairs4 = `${question4}${answer4}\n\n`
-    let pairs5 = `${question5}${answer5}\n\n`
-    let pairs = [pairs1, pairs2, pairs3, pairs4, pairs5]
-
-    let welcomeNote = `# Welcome to ${answer1} App!\n\n`
-    fs.writeFile('README.md', welcomeNote, (err) => {
-        err ? console.error(err) : console.log('Success!')
-    });
-
-    for (const pair of pairs){
-        //createFormat(question, answersAll) 
-        fs.appendFile('README.md', pair, (err) => {
-            err ? console.error(err) : console.log('Success!')
-        });
-    }
+    generateMarkdown.generateMarkdown(answers)
     
 })
 .catch((err) => {
@@ -70,33 +51,30 @@ inquirer.prompt([ // ADD LINKS readme Markdown [title](https://www.example.com) 
 })
 
 // // TODO: Create a function to initialize app
-// function init() {}
+
+// function init() { // process.argv[2]
+//     const npm = 'npm i' //string
+//     console.log(npm)
+//     console.log(generateMarkdown.init())
+// }
+
+// class initialize {
+//     constructor (dependencies){
+//         this.dependencies = ""
+//     }
+// }
 
 // // Function call to initialize app
 // init();
 
 
 
-// TODO: Create an array of questions for user input
-// let question1 = `##Project Title?\n`
-// let question2 = `##Description?\n`
-// let question3 = `##What Problem does it Solve?\n`
-// let question4 = `##How does it work?\n`
-    
-// const questionsAll = [question1, question2, question3, question4];
+
 
 // let prompt = inquirer.createPromptModule()
 // prompt(questionsAll).then()
 
-//`${H2, answer}`
 //return `${question1}, ${answer1}`
-//let pairs = `${question2}, ${answer2}`
-
-// fs.appendFile('README.md', H2, answer1 (err) =>
-//    err ? console.error(err) : console.log('Success!')
-// );
-
-// 1. questions.forEach() // append to README file 
 
 // 2. for (const question of questions){}
 
@@ -116,22 +94,4 @@ inquirer.prompt([ // ADD LINKS readme Markdown [title](https://www.example.com) 
 // 5. const readMeFile = `${data.name}.md` //.md or .json ?
 
 // TODO: Create a function to write README file = Write to Disk
-//function writeToFile(fileName, data) {}
-// function createFormat(questions, answers) {
-//     console.log(`${questions}\n${answers}`)
-// }
 
-// let H2 = `##Description:${}\n`
-
-// fs.appendFile('README.md', H2, (err) =>
-//   err ? console.error(err) : console.log('Success!')
-// );
-// fs.appendFile('README.md', JSON.stringify(data, null, '\t'), (err) => {
-//     err ? console.error(err) : console.log(data)
-// });
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
--- readMeGen-app/utils/generateMarkdown.js
+++ readMeGen-app/utils/generateMarkdown.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -11,12 +14,50 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+let generateMarkdown = (answers) => {
+  //Questions
+  let question1 = `## Project Name?\n\n`
+  let question2 = `## Description?\n\n`
+  let question3 = `## What Problem does it Solve?\n\n`
+  let question4 = `## How does it work?\n\n`
+  let question5 = `## Deployed Link: [title](https://www.example.com)'\n\n`
+  let question6 = `## Image Link: ![alt text](image.jpg)\n\n`
+  let question7 = `## License\n\n`
 
-`;
+  //Answers
+  let answer1 = answers.README1
+  let answer2 = answers.README2
+  let answer3 = answers.README3 
+  let answer4 = answers.README4 
+  let answer5 = answers.README5 
+  let answer6 = answers.README6 
+  let answer7 = answers.README7 
+
+  //Pairs
+  let pairs1 = `${question1}${answer1}\n\n`
+  let pairs2 = `${question2}${answer2}\n\n`
+  let pairs3 = `${question3}${answer3}\n\n`
+  let pairs4 = `${question4}${answer4}\n\n`
+  let pairs5 = `${question5}${answer5}\n\n`
+  let pairs6 = `${question6}${answer6}\n\n`
+  let pairs7 = `${question7}${answer7}\n\n`
+  let pairs = [pairs1, pairs2, pairs3, pairs4, pairs5, pairs6, pairs7]
+
+  let welcomeNote = `# Welcome to ${answer1} App!\n\n`
+  fs.writeFile('README.md', welcomeNote, (err) => {
+    err ? console.error(err) : console.log('Success!')
+  });
+
+  for (const pair of pairs){ 
+    fs.appendFile('README.md', pair, (err) => {
+      err ? console.error(err) : console.log('Success!')
+    });
+  }
 }
 
 
-
-module.exports = generateMarkdown;
+// add all functions 
+module.exports = {
+  generateMarkdown,
+  
+};
