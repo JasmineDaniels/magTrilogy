42100a725d7380dcbbc3af80ad9cae0060f7f1d5
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-07-04 21:37:44 -0400

    switched to writeFile

--- readMeGen-app/index.js
+++ readMeGen-app/index.js
@@ -4,53 +4,74 @@ const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown')
 //const fs = require('fs')
 
-// const readMEGen = inquirer.prompt([])
-inquirer.prompt([ 
-    {
-        type: 'input',
-        message: 'Project Name?',
-        name: 'README1',
-    },
-    {
-        type: 'input',
-        message: 'Description?',
-        name: 'README2',
-    },
-    {
-        type: 'input',
-        message: 'What Problem does it Solve?',
-        name: 'README3',
-    },
-    {
-        type: 'input',
-        message: 'How does it work??',
-        name: 'README4',
-    },
-    {
-        type: 'input',
-        message: 'Deployed Link: [title](https://www.example.com)',
-        name: 'README5',
-    },
-    {
-        type: 'input',
-        message: 'Image Link: ![alt text](image.jpg)',
-        name: 'README6',
-    },
-    {
-        type: 'input',
-        message: 'License',
-        name: 'README7',
-    },  // test
-])
-.then((answers) => {
+ // TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt([ 
+        {
+            type: 'input',
+            message: 'Project Name?',
+            name: 'README1',
+        },
+        {
+            type: 'input',
+            message: 'Description?',
+            name: 'README2',
+        },
+        {
+            type: 'input',
+            message: 'What Problem does it Solve?',
+            name: 'README3',
+        },
+        {
+            type: 'input',
+            message: 'How to use?',
+            name: 'README4',
+        },
+        {
+            type: 'input',
+            message: 'Deployed Link: [title](https://www.example.com)',
+            name: 'README5',
+        },
+        {
+            type: 'input',
+            message: 'Image Link: ![alt text](image.jpg)',
+            name: 'README6',
+        },
+        {
+            type: 'input',
+            message: 'Any Collaborators, Tutorials, etc that require Recognition?',
+            name: 'README7',
+        },
+        {
+            type: 'checkbox',
+            message: 'License?',
+            choices: ['![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)', new inquirer.Separator(), '![Javascript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)', new inquirer.Separator(), '![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)', new inquirer.Separator(), '![NodeJS](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)', new inquirer.Separator(), '![Django](https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white)'],
+            name: 'README8',
+        },
+        {
+            type: 'input',
+            message: 'Email:',
+            name: 'README9',
+        },
+        {
+            type: 'input',
+            message: 'Github:',
+            name: 'README10',
+        },  // test
+    ])
+    .then((answers) => {
     generateMarkdown.generateMarkdown(answers)
-    
-})
-.catch((err) => {
-    console.log(err)
-})
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+// // Function call to initialize app
+init();
 
 // // TODO: Create a function to initialize app
+// prompt init function 
 
 // function init() { // process.argv[2]
 //     const npm = 'npm i' //string
@@ -64,12 +85,25 @@ inquirer.prompt([
 //     }
 // }
 
-// // Function call to initialize app
-// init();
+//   for (const pair of pairs){ // change to map loop?
+    //     fs.appendFile('README.md', pair, (err) => {
+    //         err ? console.error(err) : console.log('Success!')
+    //     });
+    //   }
 
+// 4. Map Loop  
+// const writeToDisk = pairs.map(pair => {
+//     //Copy of readmeObj
+//     const newreadObj = {...pair};
+//     fs.appendFile('README.md', newreadObj, (err) => {
+//         err ? console.error(err) : console.log('Success!')
+//     });
+// })
+//   writeToDisk
 
-
-
+//   let readMEGen = `${pair0}${question1}${installList}${question2}${answer2}\n\n${question3}${answer3}\n\n
+//   ${install}${question4}${answer4}\n\n${checkout}${question5}${answer5}\n\n
+//   ${question6}${answer6}\n\n${question7}${answer7}\n\n`
 
 // let prompt = inquirer.createPromptModule()
 // prompt(questionsAll).then()
@@ -80,18 +114,23 @@ inquirer.prompt([
 
 // 3. const { message } = readmeObj
 
-// 4. const createReadmeObj = readmeObj.map(questions => {
+// 4. const writeToDisk = pairs.map(pair => {
         //     Copy of readmeObj
-        //const newreadObj = {...questions};
-        //     For Each  
+        // const newreadObj = {...pair};
+        // fs.appendFile('README.md', newreadObj, (err) => {
+        //     err ? console.error(err) : console.log('Success!')
+        // });
+        
+    //})
+
+// 5. const readMeFile = `${data.name}.md` //.md or .json ?
+
+// 6.    For Each  
         //newreadObj.forEach(question => {
             //console.log(question) //append data to readme
         //})
         // console.log(question)
         //})
-    //})
-
-// 5. const readMeFile = `${data.name}.md` //.md or .json ?
 
 // TODO: Create a function to write README file = Write to Disk
 
--- readMeGen-app/utils/generateMarkdown.js
+++ readMeGen-app/utils/generateMarkdown.js
@@ -1,29 +1,34 @@
 const fs = require('fs')
 
-
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//function renderLicenseLink(license) {} // table of contents
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 let generateMarkdown = (answers) => {
+  // The prompts ask the questions to get the answers you need
+  // The readmegen questions are the ones that are printed on the readme
+
   //Questions
-  let question1 = `## Project Name?\n\n`
+  let question1 = `## Table of Contents\n\n` //Table of Conents
   let question2 = `## Description?\n\n`
   let question3 = `## What Problem does it Solve?\n\n`
-  let question4 = `## How does it work?\n\n`
-  let question5 = `## Deployed Link: [title](https://www.example.com)'\n\n`
-  let question6 = `## Image Link: ![alt text](image.jpg)\n\n`
-  let question7 = `## License\n\n`
-
+  let question4 = `### How to Use?\n\n`
+  let question5 = `### Deployed Link: [title](https://www.example.com)\n\n`
+  let question6 = `### Image Link: ![alt text](image.jpg)\n\n`
+  let question7 = `## Credits\n\n` //Any Collaborators, Tutorials, etc that require Recognition?
+  let question8 = `## License\n\n` // choice
+  let question9 = `### Email\n\n`
+  let question10 = `### Github\n\n`
+  
   //Answers
   let answer1 = answers.README1
   let answer2 = answers.README2
@@ -31,28 +36,37 @@ let generateMarkdown = (answers) => {
   let answer4 = answers.README4 
   let answer5 = answers.README5 
   let answer6 = answers.README6 
-  let answer7 = answers.README7 
-
+  let answer7 = answers.README7
+  let answer8 = answers.README8 
+  let answer9 = answers.README9 
+  let answer10 = answers.README10 
+  
   //Pairs
-  let pairs1 = `${question1}${answer1}\n\n`
-  let pairs2 = `${question2}${answer2}\n\n`
-  let pairs3 = `${question3}${answer3}\n\n`
-  let pairs4 = `${question4}${answer4}\n\n`
-  let pairs5 = `${question5}${answer5}\n\n`
-  let pairs6 = `${question6}${answer6}\n\n`
-  let pairs7 = `${question7}${answer7}\n\n`
-  let pairs = [pairs1, pairs2, pairs3, pairs4, pairs5, pairs6, pairs7]
-
-  let welcomeNote = `# Welcome to ${answer1} App!\n\n`
-  fs.writeFile('README.md', welcomeNote, (err) => {
+  let pair0 = `# Welcome to ${answer1} App!\n\n`
+  let installList = `- [Installation](#installation)\n\n`
+  let creditsList = `- [Credits](#credits)\n\n`
+  let licenseList = `- [License](#license)\n\n`
+  let contactList = `- [Contact](#contact)\n\n`
+  let install = `## Installation\n\n`
+  let checkout = `## Go Check us out\n\n`
+  let contact = `## Contact\n\n`
+  //let pair1 = `${question1}${answer1}\n\n`
+  let pair2 = `${question2}${answer2}\n\n`
+  let pair3 = `${question3}${answer3}\n\n`
+  let pair4 = `${question4}${answer4}\n\n`
+  let pair5 = `${question5}${answer5}\n\n`
+  let pair6 = `${question6}${answer6}\n\n`
+  let pair7 = `${question7}${answer7}\n\n`
+  let pair8 = `${question8}${answer8}\n\n`
+  let pair9 = `${question9}${answer9}\n\n`
+  let pair10 = `${question10}${answer10}\n\n`
+    
+  let readMEGen = `${pair0}${question1}${installList}${creditsList}${licenseList}${contactList}${pair2}${pair3}${install}${pair4}${checkout}${pair5}${pair6}${pair7}${pair8}${contact}${pair9}${pair10}`
+    
+  //Write to disk
+  fs.writeFile('README.md', readMEGen, (err) => {
     err ? console.error(err) : console.log('Success!')
   });
-
-  for (const pair of pairs){ 
-    fs.appendFile('README.md', pair, (err) => {
-      err ? console.error(err) : console.log('Success!')
-    });
-  }
 }
 
 
