24f54023f254a73d7af87b7342eea9a470feb24c
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-07-08 12:26:49 -0400

    format answers

--- readMeGen-app/utils/generateMarkdown.js
+++ readMeGen-app/utils/generateMarkdown.js
@@ -26,12 +26,12 @@ function init() {
       },
       {
           type: 'input',
-          message: 'Deployed Link: [title](https://www.example.com)',
+          message: 'Deployed Link: https://www.example.com',
           name: 'README5',
       },
       {
           type: 'input',
-          message: 'Image Link: ![alt text](image.jpg)',
+          message: 'Image Link: ./img/example.png ',
           name: 'README6',
       },
       {
@@ -47,12 +47,12 @@ function init() {
       },
       {
           type: 'input',
-          message: 'Email:[Contact Us](mailto:admin@example.com)',
+          message: 'Email:',
           name: 'README9',
       },
       {
           type: 'input',
-          message: 'Github: [title](https://www.example.com)',
+          message: 'Github Username:',
           name: 'README10',
       },  // test
   ])
@@ -71,13 +71,13 @@ let generateMarkdown = (answers) => {
   // The generateMarkdown questions are the ones that are printed to the readme.
 
   //Questions
-  let question1 = `## Table of Contents\n\n` //Table of Conents
+  let question1 = `## Table of Contents\n\n` 
   let question2 = `## Description?\n\n`
   let question3 = `## What Problem does it Solve?\n\n`
   let question4 = `### How to Use?\n\n`
   let question5 = `### Deployed Link:\n\n`
   let question6 = `### Demo Link:\n\n`
-  let question7 = `## Credits\n\n` //Any Collaborators, Tutorials, etc that require Recognition?
+  let question7 = `## Credits\n\n` 
   let question8 = `## License\n\n` // choice
   let question9 = `### Email:\n\n`
   let question10 = `### Github:\n\n`
@@ -103,16 +103,20 @@ let generateMarkdown = (answers) => {
   let install = `## Installation\n\n`
   let checkout = `## Go Check us out\n\n`
   let contact = `## Contact\n\n`
+  let contactEmail = `[Contact Email](mailto:${answer9})`
+  let contactGithub = `[Our Github](https://github.com/${answer10})`
+  let deployedLink = `[${answer1}](${answer5})`
+  let imageLink = `![${answer1} Demo](${answer6})`
   //let pair1 = `${question1}${answer1}\n\n`
   let pair2 = `${question2}${answer2}\n\n`
   let pair3 = `${question3}${answer3}\n\n`
   let pair4 = `${question4}${answer4}\n\n`
-  let pair5 = `${question5}${answer5}\n\n`
-  let pair6 = `${question6}${answer6}\n\n`
+  let pair5 = `${question5}${deployedLink}\n\n`
+  let pair6 = `${question6}${imageLink}\n\n`
   let pair7 = `${question7}${answer7}\n\n`
   let pair8 = `${question8}${answer8}\n\n`
-  let pair9 = `${question9}${answer9}\n\n`
-  let pair10 = `${question10}${answer10}\n\n`
+  let pair9 = `${question9}${contactEmail}\n\n`
+  let pair10 = `${question10}${contactGithub}\n\n`
     
   let readMEGen = `${pair0}${question1}${installList}${creditsList}${licenseList}${contactList}${pair2}${pair3}${install}${pair4}${checkout}${pair5}${pair6}${pair7}${pair8}${contact}${pair9}${pair10}`
     
